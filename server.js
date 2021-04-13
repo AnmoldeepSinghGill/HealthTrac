49e5237d4f487b02f104692d4ae95172b23deec1
Anmoldeep Singh Gill <gillasg788@gmail.com>
2021-04-12 22:55:09 -0400

    Resolved merge conflicts

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var db = mongoose();
 // Create a new Express application instance
 var app = express();
 // Use the Express application instance to listen to the '3000' port
-app.listen(3001);
+app.listen(5000);
 // Use the module.exports property to expose our Express application instance for external usage
 module.exports = app; //returns the application object
 // Log the server status to the console
