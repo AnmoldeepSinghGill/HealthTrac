baf7ae1b4e76f65d482e463bf896c29fec08fed8
Anmoldeep Singh Gill <gillasg788@gmail.com>
2021-04-14 03:03:23 -0400

    Added cloud database and added ui element to nurse and patient dashboard

--- react-client/src/component/pages/patientDashboard.js
+++ react-client/src/component/pages/patientDashboard.js
@@ -1,5 +1,6 @@
 import { useContext, useEffect } from 'react';
 import AuthContext from '../../context/auth/authContext';
+import "../../App.css";
 
 const PatientDashboard = (props) => {
     const authContext = useContext(AuthContext);
@@ -13,6 +14,18 @@ const PatientDashboard = (props) => {
     return (
         <div>
             <h1>Patient Dashboard</h1>
+            <div className="card-container">
+                <div className="container-fluid">
+                    <div className="row justify-content-center">
+                        <h2>Today's Motivation</h2>
+                    </div>
+                    <div className="row justify-content-center">
+                        <iframe width="100%" height="500px"
+                                src="https://www.youtube.com/embed/ZXsQAXx_ao0">
+                        </iframe>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
